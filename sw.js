f82859c309b882e4cf997288fb72092343669cd4
Damien <DamienMH@users.noreply.github.com>
2026-09-12 10:29:33 +0200

    arrivee sur une carte constatee par l observateur, plus par la position

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
 // A incrementer des que la coquille change : l activation efface les anciens
 // caches, sinon un index.html perime resterait servi indefiniment.
 // v2 : ouverture animee et illustrations.
-const CACHE = "feed-c37c87aa";
+const CACHE = "feed-506feece";
 const COQUILLE = ["./", "./index.html", "./manifest.json"];
 
 self.addEventListener("install", (e) => {
