4168fce70900a61438203fab3d6f675f3a381fc2
Damien <DamienMH@users.noreply.github.com>
2026-09-12 09:58:31 +0200

    feed : le corps du texte survit au degraissage, resumes au format carte

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
 // A incrementer des que la coquille change : l activation efface les anciens
 // caches, sinon un index.html perime resterait servi indefiniment.
 // v2 : ouverture animee et illustrations.
-const CACHE = "feed-9ec558ac";
+const CACHE = "feed-7b867b77";
 const COQUILLE = ["./", "./index.html", "./manifest.json"];
 
 self.addEventListener("install", (e) => {
